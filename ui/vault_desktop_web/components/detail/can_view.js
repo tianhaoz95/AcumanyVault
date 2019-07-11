8c883ec27c1c147c2c37ea6e5c0f5a215e7df57e
Tianhao Zhou <tianhaoz@umich.edu>
2019-07-11 17:36:03 +0000

    dev(ui): add list of chips

--- ui/vault_desktop_web/components/detail/can_view.js
+++ ui/vault_desktop_web/components/detail/can_view.js
@@ -1,9 +1,47 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Chip from '@material-ui/core/Chip';
+import { makeStyles } from '@material-ui/core/styles';
 
-function CanView() {
+const useStyles = makeStyles(theme => ({
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: 200,
+  },
+  button: {
+    margin: theme.spacing(1),
+  },
+  chip: {
+    margin: theme.spacing(1),
+  }
+}));
+
+function CanView(props) {
+    const classes = useStyles();
+    const can_view_list = [1,1,1,1,1];
+    const chip_list = can_view_list.map((idx) => (
+        <Chip
+            key={idx}
+            label="Basic Chip"
+            className={classes.chip}
+        />
+    ));
     return (
         <React.Fragment>
-            View
+            <TextField
+                className={classes.textField}
+                label="Key"
+                variant="outlined"
+            />
+            <Button
+                variant="contained"
+                className={classes.button}
+                >
+                Add
+            </Button>
+            {chip_list}
         </React.Fragment>
     );
 }
