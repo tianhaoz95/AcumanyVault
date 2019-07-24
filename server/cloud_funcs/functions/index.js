96cab4c1be44452cf06b42eda62664bf35f47e73
Tianhao Zhou <tianhaoz@umich.edu>
2019-07-24 17:11:29 +0000

    dev: add user creation backend

--- server/cloud_funcs/functions/index.js
+++ server/cloud_funcs/functions/index.js
@@ -1,8 +1,20 @@
+const admin = require('firebase-admin');
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+admin.initializeApp(functions.config().firebase);
+
+let db = admin.firestore();
+
 exports.helloWorld = functions.https.onRequest((request, response) => {
     response.send("Hello from Firebase!");
+});
+
+exports.createUser = functions.https.onRequest((request, response) => {
+  let docRef = db.collection('users').doc('alovelace');
+
+  let setAda = docRef.set({
+    first: 'Ada',
+    last: 'Lovelace',
+    born: 1815
+  });
 });
